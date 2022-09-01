a84e0e13d46efa5e8140b7db838635713699a903
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-09-01 17:37:12 -0400

    Time: 2 ms (33.92%), Space: 43.3 MB (85.35%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -1,18 +1,39 @@
-public class Solution {
-private boolean result = true;
-
-public boolean isBalanced(TreeNode root) {
-    maxDepth(root);
-    return result;
-}
-
-public int maxDepth(TreeNode root) {
-    if (root == null)
-        return 0;
-    int l = maxDepth(root.left);
-    int r = maxDepth(root.right);
-    if (Math.abs(l - r) > 1)
-        result = false;
-    return 1 + Math.max(l, r);
-}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isBal = true;
+    public boolean isBalanced(TreeNode root) {
+        
+        helper(root);
+        
+        return isBal;
+    }
+    
+    private int helper(TreeNode root)
+    {
+        if(root == null)return 0;
+        
+        int lh = helper(root.left);
+        int rh = helper(root.right);
+        
+        if(Math.abs(lh - rh) > 1)
+        {
+            isBal = false;
+        }
+        
+        return 1+ Math.max(lh, rh);
+    }
 }
