8893954d8451ab77d02ba3f6fba286ec94b7f44b
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-09-01 16:44:55 -0400

    Time: 2 ms (35.87%), Space: 43.9 MB (19.85%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,7 +15,9 @@
  */
 class Solution {
     
-    List<List<Integer>> res = new ArrayList<>();
+    
+    /*********************RECURSIVE(DFS)*******************/
+/*    List<List<Integer>> res = new ArrayList<>();
     
     public List<List<Integer>> levelOrder(TreeNode root) {
         
@@ -28,7 +30,7 @@ class Solution {
     
     private void helper(TreeNode root, int level)
     {
-        if(level == res.size())//why this works? bcoz level we increased in last 
+        if(level == res.size())//why this works? bcoz level we inc the level in each left/right subtree call, and the res' size is increased in each level + we make entries and the level is inc for the next right/left subtree HENCE when increased it is equal to res' size therefore we add another level and SO ON...
         {
             res.add(new ArrayList<>());
         }
@@ -45,4 +47,45 @@ class Solution {
             helper(root.right, level + 1);
         }
     }
+    //RECURSIVE ENDS
+    */
+    
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        if(root == null)return res;
+        
+        q.add(root);
+        
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            
+            List<Integer> currLevel = new ArrayList<>();
+            for(int i = 0; i < size; ++i)//each for loop for 1 level i.e. 9, 20 in one for loop and then their added children in next while iteration's for loop
+            {
+                TreeNode node = q.remove();
+                currLevel.add(node.val);
+                
+                if(node.left != null)
+                {
+                    q.add(node.left);
+                }
+                if(node.right != null)
+                {
+                    q.add(node.right);
+                }
+            }
+            res.add(currLevel);
+        }
+        
+        return res;
+        
+    }
+    
+    
 }
