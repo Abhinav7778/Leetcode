49b9b992fcaf02c1ee598cc662f867e02f7fc68b
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-09-04 17:44:47 -0400

    Time: 0 ms (100.00%), Space: 39.9 MB (94.37%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -1,7 +1,6 @@
 class Solution {
     public String tictactoe(int[][] moves) {
         int player = -1;
-        int xsumx = 0, xsumy = 0, osumx = 0, osumy = 0, xcount = 0, ycount = 0;
         
         int row[] = new int[3];
         int col[] = new int[3];
@@ -41,21 +40,6 @@ class Solution {
         }
         
 
-                
-        for(int i = 0; i < row.length; ++i)
-        {
-            System.out.print("Row "+row[i]);
-        }
-        
-                        
-        for(int i = 0; i < col.length; ++i)
-        {
-            System.out.print("Col "+col[i]);
-        }
-        
-        System.out.print("Diag  "+diag);
-        
-        System.out.print("AntiDiag  "+antiDiag);
         if(row[0] == 3 || row[1] == 3 || row[2] == 3 || col[0] == 3  || col[1] == 3 || col[2] == 3 || diag == 3 || antiDiag == 3)
             return "A";
         if(row[0] == -3 || row[1] == -3 || row[2] == -3 || col[0] == -3  || col[1] == -3 || col[2] == -3  || diag == -3 || antiDiag == -3)
