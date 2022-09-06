727376db6da901735e171b99e4e9bce1ce7d656f
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-09-06 16:08:07 -0400

    Time: 3 ms (5.63%), Space: 42.3 MB (82.19%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -1,26 +1,46 @@
-class Solution 
-{
-    public void flatten(TreeNode root)
-    {//preorder iterative(Root Left Right)
-        if(root == null)//base case for null graph 
-            return;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+   public void flatten(TreeNode root) {
         
-        ArrayDeque<TreeNode> stack= new ArrayDeque<>();//Faster than Stack 
+        if(root == null)return;
+       
+        Stack<TreeNode> st = new Stack<>();
         
-        stack.addLast(root);//adding the root node 
-        while(!stack.isEmpty())//terminating condition 
+        st.push(root);
+        
+        while(!st.isEmpty())
         {
-            TreeNode temp= stack.removeLast();//popping the top of the stack, the inorder root//Root
+            TreeNode node = st.pop();
             
-            if(temp.right != null)
-                stack.addLast(temp.right);//we are pushing the right first to access the the left fast //Right
-            if(temp.left != null)
-                stack.addLast(temp.left);//we are pushing the left at last to access it faster as it is the next preorder element //Left
+            if(node.right != null)
+            {
+                st.push(node.right);
+            }
+            if(node.left != null)
+            {
+                st.push(node.left);
+            }
             
-            if(!stack.isEmpty())
-               temp.right= stack.peekLast();//only item left on the top of the stack is the next inorder root, so creating a dependency linking each other 
-            temp.left= null;//destroying the left link and pointing to the null
+            if(!st.isEmpty())
+            {
+                node.right = st.peek();
+            }
+            node.left = null;
         }
-        return;//returning to the main  method 
     }
-}//please do Upvote, it helps a lot
+}
