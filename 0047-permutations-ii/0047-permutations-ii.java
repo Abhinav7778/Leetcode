51acc21cbc13dc195dcc6e2a37bc063989995c78
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-11-08 17:50:42 -0500

    Time: 1 ms (100.00%), Space: 43.3 MB (84.94%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(res, ans, nums, 0, new boolean[nums.length]);
+        
+        return res;
+    }
+    
+    public void helper(List<List<Integer>> res, List<Integer> ans, int[] nums, int start, boolean[] used)
+    {
+        if(ans.size() == nums.length)
+            res.add(new ArrayList<>(ans));
+        else
+        {
+            for(int i = 0; i < nums.length; ++i)
+                {
+                    if(used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i-1]))continue;
+                    used[i] = true;
+                    ans.add(nums[i]);
+                    helper(res, ans, nums, i + 1, used);
+                    used[i] = false;
+                    ans.remove(ans.size() - 1);
+                }
+        }
+        
+
+    }
+}
