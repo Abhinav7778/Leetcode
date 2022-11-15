1661b450b31f397e04997073bbe560372b4e57f8
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-11-15 14:27:35 -0500

    Time: 1 ms (100.00%), Space: 43.8 MB (93.37%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,10 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0; i < nums.length; ++i)
+        
+        /*The crux over here is that the array is already sorted.
+We are comparing the first and last elements because after square these have the possibility of being the highest element.
+Both the extremes contain the max element (after square ofc), so we are inserting these elements to the last of the new array to make it sorted.*/
+        
+        int i = 0, j = nums.length - 1;
+        
+        int[] res = new int[nums.length];
+        
+        int st = res.length - 1;
+        while(i <= j)
         {
-            nums[i] = nums[i] * nums[i];
+            if(Math.abs(nums[i]) > Math.abs(nums[j]))
+            {
+                res[st] = nums[i] * nums[i];
+                ++i;
+            }
+            else
+            {
+                res[st] = nums[j] * nums[j];
+                --j;
+            }
+            --st;
         }
-        Arrays.sort(nums);
-        return nums;
+        return res;
     }
 }
