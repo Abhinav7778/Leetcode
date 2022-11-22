481a98de1ce9f601541810247c094dbc1abb4aab
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-11-22 15:06:19 -0500

    Time: 5 ms (45.94%), Space: 41.3 MB (36.95%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,91 @@
+
+
+class Solution {
+    public int totalNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        
+        // n queens : N*N board
+        // initializing the board with "."
+
+        
+       char[][] temp = new char[n][n];
+        
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                temp[i][j] = '.';
+            }
+        }
+        
+        helper(res, temp, n, 0);
+        
+        return res.size();
+    }
+    
+    public void helper(List<List<String>> res, char[][] ans, int n, int row)
+    {
+        if(row == n)
+        {
+            res.add(charToList(ans));
+            return;
+        }
+        
+        for(int col = 0; col < n; ++col)
+        {
+            if(isValidCol(ans, n, row, col))
+            {
+                ans[row][col] = 'Q';
+                helper(res, ans, n, row + 1);
+                ans[row][col] = '.';
+            }
+        }
+    }
+    
+    public boolean isValidCol(char[][] ans, int n, int row, int col)
+    {
+        
+        //check if the col had a queen before
+        for(int i = 0; i < n; ++i)
+        {
+            if(ans[i][col] == 'Q')
+            {
+                return false;
+            }
+        }
+        
+        
+        //checking diagonal
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+        {
+            if(ans[i][j] == 'Q')
+            {
+                return false;
+            }
+        }
+        
+        
+        //checking anti-diagonal
+        
+        for(int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+        {
+            if(ans[i][j] == 'Q')
+                return false;
+        }
+        
+        return true;
+        
+    }
+    
+    public List<String> charToList(char[][] ans)
+    {
+        List<String> res = new ArrayList<>();
+        
+        for(char[] c : ans)
+        {
+            res.add(new String(c));
+        }
+        
+        return res;
+    }
+}
