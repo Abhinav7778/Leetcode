ae3169b086dfc2e4aa72b13812df47933164e5af
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2022-11-25 13:26:51 -0500

    Time: 71 ms (59.81%), Space: 75.1 MB (57.44%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,46 +1,25 @@
 class Solution {
-    
-  //  TC: O(N log N)
-//     public int longestConsecutive(int[] nums) {
-//         Arrays.sort(nums);
-
-//         if(nums.length == 0)return 0;
-//         int count = 1, max = Integer.MIN_VALUE;
-//         for(int i = 1; i < nums.length; ++i)
-//         {
-//             if(nums[i] == nums[i-1])
-//             {
-//                 continue;
-//             }
-//             if(nums[i] == nums[i-1] + 1)
-//             {
-//                 ++count;
-//                 max = Math.max(max, count);
-//             }
-//             else
-//             {
-//                 count = 1;
-//             }
-//         }
-//         return max == Integer.MIN_VALUE ? 1 : max;
-//     }
-// }
-
-  public int longestConsecutive(int[] nums) {
-            Set<Integer> set = new HashSet<>();
-            for(int n : nums) {
-                set.add(n);
-            }
-            int best = 0;
-            for(int n : set) {
-                if(!set.contains(n - 1)) {  // only check for one direction
-                    int m = n + 1;
-                    while(set.contains(m)) {
-                        m++;
-                    }
-                    best = Math.max(best, m - n);
-                }
-            }
-            return best;
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> hs = new HashSet<>();
+        int res = Integer.MIN_VALUE;
+        for(int i : nums)
+        {
+            hs.add(i);
         }
+        
+        for(int i : hs)
+        {
+            if(!hs.contains(i-1))
+            {
+                int j = i + 1;
+                while(hs.contains(j))
+                {
+                    ++j;
+                }
+                res = Math.max(res, j-i);
+            }
+        }
+        
+        return res == Integer.MIN_VALUE ? 0 : res;
+    }
 }
