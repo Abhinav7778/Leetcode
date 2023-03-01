63823bb54c83496641ffc8be7fd82bcf987595b8
Abhinav7778 <89945897+Abhinav7778@users.noreply.github.com>
2023-03-01 12:22:36 -0500

    Time: 275 ms (19.77%), Space: 42.7 MB (46.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        int maxl = Integer.MIN_VALUE;
-                    int st = 0, e = 0;
-
+        
+        String res = "";
+        int max = Integer.MIN_VALUE;
         for(int i = 0; i < s.length(); ++i)
         {
-            int l1 = fromCenter(s, i, i);
-            int l2 = fromCenter(s, i, i + 1);
+            int l1 = fromCenter(i, i, s);
+            int l2 = fromCenter(i, i + 1, s);
             
             int l = Math.max(l1, l2);
             
-            if( l > maxl)
+            // System.out.print(" l " + l);
+            int st = (i - (l-1)/2);
+            // System.out.print(" st " + st);
+            
+            int e = i + l/2;
+            // System.out.print(" e " + e );
+            
+            if(e - st + 1 > max)
             {
-                st = i - (l-1)/2;
-                e = i + l/2;
-                maxl = l;
+                res = s.substring(st, e + 1);
+                max = e - st + 1;
             }
         }
         
-        return s.substring(st, e + 1);
-        
+        return res;
     }
-    public int fromCenter(String s, int st, int e)
+    
+    public int fromCenter(int st, int e, String s)
     {
-        while(st >= 0 && e < s.length() && s.charAt(st) == s.charAt(e))
+        // System.out.println(" st char " + s.charAt(st) + " st " + st + " e char "+s.charAt(e) +" e" + e  );
+        while(st >= 0 && e < s.length() && s.charAt(st) == s.charAt(e) )
         {
+                                // System.out.println(" st char " + s.charAt(st) + " st " + st + " e char "+s.charAt(e) +" e" + e  );
+
             --st;
             ++e;
+
         }
+        System.out.println();
         return e - st - 1;
     }
 }
